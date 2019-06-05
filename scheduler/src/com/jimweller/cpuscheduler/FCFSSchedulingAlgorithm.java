de4d58de56d21223660366d5ffc148af355258dc
Timothy Lin <timothylin1199@gmail.com>
2019-06-05 15:16:45 -0700

    Deleted MemoryBlock file and moved class to FCFS

--- scheduler/src/com/jimweller/cpuscheduler/FCFSSchedulingAlgorithm.java
+++ scheduler/src/com/jimweller/cpuscheduler/FCFSSchedulingAlgorithm.java
@@ -8,6 +8,56 @@ package com.jimweller.cpuscheduler;
 
 import java.util.*;
 
+class MemoryBlock{
+
+    private long size;
+    private boolean free;
+    private Process p;
+
+    public MemoryBlock(Process ap){
+        size = 0;
+        free = true;
+        p = ap;
+    }
+
+    public MemoryBlock(long aSize, Process ap){
+        size = aSize;
+        free = true;
+        p = ap;
+    }
+
+    public MemoryBlock(long aSize, boolean aFree, Process ap){
+        size = aSize;
+        free = aFree;
+        p = ap;
+    }
+
+    public Process getProcess(){
+        return p;
+    }
+
+    public void setProcess(Process ap){
+        p = ap;
+    }
+
+    public long getSize(){
+        return size;
+    }
+
+    public boolean getFree(){
+        return free;
+    }
+
+    public void setSize(long aSize){
+        size = aSize;
+    }
+
+    public void setFree(boolean aFree){
+        free = aFree;
+    }
+}
+
+
 public class FCFSSchedulingAlgorithm extends BaseSchedulingAlgorithm {
 
 	private ArrayList<Process> jobs;
